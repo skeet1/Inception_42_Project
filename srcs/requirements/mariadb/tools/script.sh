b42deab842e608712e08e05c4cff793fa19a28f7
Mohamed KARIM <karimkar999@gmail.com>
2023-06-05 09:18:26 +0100

    Before reset

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-echo "Hello Himda"
+# Start the MariaDB service
+service mysql start
 
+# Wait for the service to initialize
+sleep 5
+
+# Create the database
+mysql -e "CREATE DATABASE skeet_db;"
+
+# Create a user and grant privileges
+mysql -e "CREATE USER 'skeet'@'%' IDENTIFIED BY 'skeet_pass';"
+mysql -e "GRANT ALL PRIVILEGES ON skeet_db.* TO 'skeet'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Stop the MariaDB service
+service mysql stop
+
+# Run the default command (mysqld_safe)
 exec "$@"
