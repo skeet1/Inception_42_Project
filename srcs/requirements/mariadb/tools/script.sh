bf123eda412ac7b0a4a305ddf7a387789be844f5
Mohamed KARIM <karimkar999@gmail.com>
2023-07-07 21:34:30 +0100

    Last update on mariadb teaks in dockerfile and script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-# Start the MariaDB service
+sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 service mysql start
 
-# Wait for the service to initialize
-sleep 5
-
-# Create the database
-mysql -e "CREATE DATABASE skeet_db;"
-
-# Create a user and grant privileges
+mysql -e "CREATE DATABASE IF NOT EXISTS SKEET_DB;"
 mysql -e "CREATE USER 'skeet'@'%' IDENTIFIED BY 'skeet_pass';"
 mysql -e "GRANT ALL PRIVILEGES ON skeet_db.* TO 'skeet'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 
-# Stop the MariaDB service
 service mysql stop
 
-# Run the default command (mysqld_safe)
-exec "$@"
+mysqld
