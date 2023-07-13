e7a9619120b99121766f2bd580e791df40353504
Mohamed KARIM <karimkar999@gmail.com>
2023-07-13 18:55:34 +0100

    Edit on script for nginx

--- srcs/requirements/nginx/tools/install_tools.sh
+++ srcs/requirements/nginx/tools/install_tools.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo "Let's create TLS certificate"
+# these lines will explain to you all arguments needed for openssl
+#   req hiya li kadir create and processing les certificate request
+#   -x509 type of certif
+#   now if we run the openssl will request for password but -nodes flag prevent that and make the request continue without a password
+#   -out where crt and key should be
+#   -subj for all other needed informations
 openssl req -x509 -nodes -out /etc/nginx/MyCertificate.crt \
     -keyout /etc/nginx/MyKey.key -subj \
     "/C=MA/ST=myState/L=Casablanca/O=myCompany/OU=mySection/CN=mkarim.42.fr"
